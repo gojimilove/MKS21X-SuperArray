e49a7fefe119e89f5cad725f46e267d37a93ffc4
Jessica Kim <jkim00@stuy.edu>
2018-10-24 00:12:39 -0400

    phase one of SuperArray finished, lots of errors but i'll work them out later, Driver added (not mine)

--- SuperArray.java
+++ SuperArray.java
@@ -15,25 +15,47 @@ public class SuperArray{
   }
 
   public boolean isEmpty() {
-    if (size == 0) return true;
+    if (size() == 0) return true;
     return false;
   }
 
   public boolean add(String new) {
-    if (size == data.length) resize();
-    else data[size] = new;
+    data[size()] = new;
     return true;
   }
 
   public String toString() {
-    String result = "["
-    for (int i = 0; i < size; i++) {
+    String result = "[";
+    for (int i = 0; i < size(); i++) {
       result+= data[i];
     }
     result += "]";
     return result;
   }
 
-  public String toStringDebug()
+  public String toStringDebug() {
+  	String result = "[";
+    for (int i = 0; i < data.length; i++) {
+    	if (data[i] == null) result+= "null, ";
+      else result = result + data[i] + ", ";
+    }
+    result += "]";
+    return result;
+  }
+
+  public String get(int index) {
+  	if (index < 0 || index >= size()) return null;
+  	return data[index];
+  }
+
+  public String set(int index, String new) {
+  	String old = "";
+  	if (index < 0 || index >= size()) return null;
+  	else {
+  		old = data[index];
+  		data[index] = new;
+  		return old;
+  	}
+  }
 
 }
